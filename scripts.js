a0ee03ef4a62655688ca5bc6dab14489a81029f6
Johnny <jonesmarjr@gmail.com>
2025-11-09 17:29:04 -0300

    primeiraversao

---
+++ scripts.js
@@ -0,0 +1,22 @@
+const videoww = document.getElementById("videoww")
+const videodm = document.getElementById("videodm")
+const videosn = document.getElementById("videosn")
+videoww.addEventListener('mouseover', function () {
+    return videoww.play();
+})
+videoww.addEventListener('mouseout', function () {
+    return videoww.load();
+})
+videodm.addEventListener('mouseover', function () {
+    return videodm.play(), videodm.volume = 0.03;
+})
+videodm.addEventListener('mouseout', function () {
+    return videodm.load();
+})
+videosn.addEventListener('mouseover', function () {
+    return videosn.play();
+})
+videosn.addEventListener('mouseout', function () {
+    return videosn.load();
+})
+
